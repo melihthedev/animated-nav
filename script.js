301a29cb9ef244f74e302275b281af076f581560
Melih Çetin <69591458+melihthedev@users.noreply.github.com>
2021-01-30 21:53:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById("nav")
+const toggleBtn = document.getElementById("toggle")
+
+toggleBtn.addEventListener("click", () => {
+    nav.classList.toggle("active")
+})
